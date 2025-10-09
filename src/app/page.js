c8f5519a84d46f09f6070bbf239ca8edca7800d9
Shivanitech777 <sssstdnt5@gmail.com>
2025-10-09 15:49:56 +0530

    remove unused imports from contact and home pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,8 @@
 
 import DemoHero from "@/components/DemoHero";
 import Count from "@/components/Count";
-import TargetSection from "@/components/Target";
 import AboutPage from "@/components/About";
-import ProcessSection from "@/components/ui/process";
 import FAQAndConsultation from "@/components/Factual";
-import PracticeAreas from  "@/components/PracticeAreas";
-import MissionValues from "@/components/MissionValues";
-
 
 
 export default function Home() {
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Briefcase, Scale, BookOpen } from "lucide-react";
 import MissionValues from "./MissionValues";
 import Count from "./Count";
 import TargetSection from "./Target";
